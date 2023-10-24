8865ddcc7ab2a7a6b923dd3f3704b883666e5adb
Erasto1302 <142833651+Erasto1302@users.noreply.github.com>
2023-10-24 15:46:11 -0300

    Third

--- app/Menu/page.tsx
+++ app/Menu/page.tsx
@@ -1,20 +1,12 @@
 export default function Menu() {
     return (
         <>
-        <nav>
-            <p></p>
-
-            <ul>
-                <li>Home</li>
-                <li>Quem Somos</li>
-                <li>Portofólio</li>
-                <li>Produtos</li>
-                
-            </ul>
-        </nav>
-
-        
-        
+         <ul>
+            <li>Home</li>
+            <li>Quem Somos</li>
+            <li>Portofólio</li>
+            <li>Produtos</li>
+         </ul>
         </>
 
     )

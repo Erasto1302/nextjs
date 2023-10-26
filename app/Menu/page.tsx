60480223fedd8687d5e007857d7d8d27345db1c9
Erasto1302 <142833651+Erasto1302@users.noreply.github.com>
2023-10-26 16:15:19 -0300

    FIRST Comp

--- app/Menu/page.tsx
+++ app/Menu/page.tsx
@@ -1,12 +1,19 @@
+import { HiacdemiccCap, HiOutlineHome, Hiarrowright, HiBugAnt } from "react-icon/h2"
 export default function Menu() {
     return (
         <>
-         <ul>
-            <li>Home</li>
-            <li>Quem Somos</li>
-            <li>Portofólio</li>
-            <li>Produtos</li>
-         </ul>
+        <div className="flex bg-amber-500 justify-between pl-20 pr-20 pt-2 pb2">
+        <p className="inline-flex items-center">Bug<HiBugAnt size=(60)/> ANT</p>
+        <ul className="flex items-center">
+            <HiOutlineHome />
+            <li className="inline-flex pr-10 pl-2 items-center"> /Menu</li>
+            <li className="inline-flex pr-10"> /Menu</li>
+            <HiacdemiccCap />
+            <li className="inline-flex pr-10 pl-2 "> /Menu</li>
+            <li className="inline-flex pr-10"> /Menu</li>
+            <li className="inline-flex"></Menu</li>
+        </ul>
+        </div>
         </>
 
     )

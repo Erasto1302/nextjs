cef5bcc2a397153a05ea6d5ee3528b9f0bb7cf06
Erasto1302 <142833651+Erasto1302@users.noreply.github.com>
2023-10-26 19:54:39 +0000

    Sixth Comp

--- app/Menu/page.tsx
+++ app/Menu/page.tsx
@@ -2,8 +2,8 @@ import { HiAcademicCap, HiOutlineHome, HiBugAnt } from 'react-icons/hi2'
 export default function Menu() {
     return (
         <>
-        <div className="flex bg-amber-500 justify-between pl-20 pr-20 pt-2 pb2">
-        <p className="inline-flex items-center">Bug<HiBugAnt size={60}/> ANT</p>
+        <div className="flex bg-amber-580 justify-between pl-20 pr-20 pt-2 pb2">
+        <p className="inline-flex items-center">Bug<HiBugAnt size={70}/> ANT</p>
         <ul className="flex items-center">
             <HiOutlineHome />
             <li className="inline-flex pr-10 pl-2 items-center"> Menu</li>
